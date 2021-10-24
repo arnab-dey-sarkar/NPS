7ca07a9e4bdbc21ff63d1392bbfaeb3543a56449
arnab-dey-sarkar <38815293+arnab-dey-sarkar@users.noreply.github.com>
2021-10-24 13:04:44 +0530

    Updated MVC Configuration

--- src/main/java/com/capgemini/nps/NpsApplication.java
+++ src/main/java/com/capgemini/nps/NpsApplication.java
@@ -2,12 +2,23 @@ package com.capgemini.nps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
-public class NpsApplication {
+public class NpsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NpsApplication.class, args);
+	}
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
 	}
 
 }
